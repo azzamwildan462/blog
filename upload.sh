50e467335b8bbedb2f875aff3bbaa21f3bd5237e
azzamwildan462 <azzamwildan5@gmail.com>
2021-08-28 21:00:01 +0700

    Update_28082021

--- upload.sh
+++ upload.sh
@@ -4,3 +4,10 @@ git add .
 git commit -m "$nama_commit"
 git push origin main
 
+#log_save
+date=$(date +%d/%m/%Y)
+time=$(date +%H.%M.%S)
+who=$(whoami)
+log="Last push: $date at $time by $who"
+echo $log >> log.txt
+
