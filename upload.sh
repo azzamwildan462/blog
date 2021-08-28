fa91827989369b838760ab045df761353f7fda99
azzamwildan462 <azzamwildan5@gmail.com>
2021-08-28 21:32:26 +0700

    Update_28082021

--- upload.sh
+++ upload.sh
@@ -8,6 +8,8 @@ git push origin main
 date=$(date +%d/%m/%Y)
 time=$(date +%H.%M.%S)
 who=$(whoami)
-log="Last push: $date at $time by $who"
+os=$(hostnamectl | grep Operating\ System | awk '{print $3}')
+os_version=$(hostnamectl | grep Operating\ System | awk '{print $4}')
+log="Last push: $date at $time by $who@$os-$os_version"
 echo $log >> log.txt
 
