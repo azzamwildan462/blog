22c8eace1f5eb727f95072333cf2fb6a6e91947c
azzamwildan462 <azzamwildan5@gmail.com>
2021-08-28 21:33:49 +0700

    Update_28082021

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
 date=$(date +%d/%m/%Y)
 time=$(date +%H.%M.%S)
 who=$(whoami)
-log="Last pull: $date at $time by $who"
+os=$(hostnamectl | grep Operating\ System | awk '{print $3}')
+os_version=$(hostnamectl | grep Operating\ System | awk '{print $4}')
+log="Last pull: $date at $time by $who from $os-$os_version"
 echo $log >> log.txt
 git pull origin main
--- upload.sh
+++ upload.sh
@@ -10,6 +10,6 @@ time=$(date +%H.%M.%S)
 who=$(whoami)
 os=$(hostnamectl | grep Operating\ System | awk '{print $3}')
 os_version=$(hostnamectl | grep Operating\ System | awk '{print $4}')
-log="Last push: $date at $time by $who@$os-$os_version"
+log="Last push: $date at $time by $who from $os-$os_version"
 echo $log >> log.txt
 
