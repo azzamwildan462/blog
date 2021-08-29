93a9aad371342ea470e5389c5569ea779df59fd4
azzamwildan462 <azzamwildan5@gmail.com>
2021-08-29 09:42:40 +0700

    Update_29082021

--- upload.sh
+++ upload.sh
@@ -12,15 +12,14 @@ init_system=$(ps 1 | grep 1 | awk '{print $5}')
 
 if [[ $init_system -eq "init" ]]
 then
-    # echo "using default init (maybe SysV) system"
+    # echo "free-systemd"
     os=$(inxi -S | grep Distro | awk '{print $2}')
-    log="Last pull: $date by $who from $os at $time"
-elif [[ $init_system -eq "systemd" ]]
-then
-    # echo "using systemd init system"
+    log="Last push: $date by $who from $os (free-systemd) at $time"
+else
+    # echo "use systemd"
     os=$(hostnamectl | grep Operating\ System | awk '{print $3}')
     os_version=$(hostnamectl | grep Operating\ System | awk '{print $4}')
-    log="Last pull: $date by $who from $os-$os_version at $time "
+    log="Last push: $date by $who from $os-$os_version (systemd) at $time "
 fi
 echo $log >> log.txt
 
