1e0949b7b67fde442572d3dc6108653f5b56372f
azzamwildan462 <azzamwildan5@gmail.com>
2021-08-29 09:31:04 +0700

    Update_29082021

--- upload.sh
+++ upload.sh
@@ -8,8 +8,19 @@ git push origin main
 date=$(date +%d/%m/%Y)
 time=$(date +%H.%M.%S)
 who=$(whoami)
-os=$(hostnamectl | grep Operating\ System | awk '{print $3}')
-os_version=$(hostnamectl | grep Operating\ System | awk '{print $4}')
-log="Last push: $date at $time by $who from $os-$os_version"
+init_system=$(ps 1 | grep 1 | awk '{print $5}')
+
+if [[ $init_system -eq "init" ]]
+then
+    # echo "using default init (maybe SysV) system"
+    os=(inxi -S | grep Distro | awk '{print $2}')
+    log="Last pull: $date by $who from $os at $time"
+elif [[ $init_system -eq "systemd" ]]
+then
+    # echo "using systemd init system"
+    os=$(hostnamectl | grep Operating\ System | awk '{print $3}')
+    os_version=$(hostnamectl | grep Operating\ System | awk '{print $4}')
+    log="Last pull: $date by $who from $os-$os_version at $time "
+fi
 echo $log >> log.txt
 
