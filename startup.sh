834ea8fc432e5666f814c411161960dbe11256db
azzamwildan462 <azzamwildan5@gmail.com>
2021-08-29 09:59:54 +0700

    Update_29082021

--- startup.sh
+++ startup.sh
@@ -5,11 +5,11 @@ init_system=$(ps 1 | grep 1 | awk '{print $5}')
 
 if [[ $init_system -eq "init" ]]
 then
-    # echo "using default init (maybe SysV) system"
+    # echo "free-systemd"
     os=$(inxi -S | awk '{for(i=1;i<=NF;i++)if($i=="Distro:")print $(i+1)}')
     log="Last pull: $date by $who from $os (free-systemd) at $time"
 else
-    # echo "using systemd init system"
+    # echo "using systemd"
     os=$(hostnamectl | grep Operating\ System | awk '{print $3}')
     os_version=$(hostnamectl | grep Operating\ System | awk '{print $4}')
     log="Last pull: $date by $who from $os-$os_versio (systemd) at $time "
